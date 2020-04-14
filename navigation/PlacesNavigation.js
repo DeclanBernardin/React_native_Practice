2f85bb7c48fb9a975e35a732631eeab37a91d4b8
Declan Bernardin <dmjbernardin@yahoo.com>
2020-04-14 14:51:57 -0500

    first commit

---
+++ navigation/PlacesNavigation.js
@@ -0,0 +1,6 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation'; 
+
+import PlacesListScreen from '../screens/PlacesListScreen';
+import PlaceDetailScreen from '../screens/PlaceDetailScreen';
+import NewPlaceScreen from '../screens/NewPlaceScreen'; 
+import MapScreen from '../screens/MapScreen'; 
